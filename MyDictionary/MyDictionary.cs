a7ec827c77ea503413316bee026d6f90605a9dd7
umutmtl <umutmutlu@anadolu.edu.tr>
2021-02-20 17:56:35 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyOwnDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private TKey[] _keys;
+        private TValue[] _values;
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = _keys;
+            _keys = new TKey[_keys.Length + 1];
+
+            TValue[] tempValues = _values;
+            _values = new TValue[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+                _values[i] = tempValues[i];
+
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+        public int Count => _keys.Length;
+
+        public void List()
+        {
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                Console.WriteLine("KEY is : " + _keys[i] + " VALUE is : " + _values[i]);
+            }
+        }
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyOwnDictionary;
+using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary1 = new Dictionary<int, string>();
+            dictionary1.Add(3,"Ali");
+            dictionary1.Add(14, "Mehmet");
+            //Console.WriteLine(dictionary1.GetValueOrDefault(3));
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(6,"AHMET");
+            myDictionary.Add(15,"MEHMET");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.List();
+        }
+    }
+}
